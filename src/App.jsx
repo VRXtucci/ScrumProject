beadde8b0c085855dad0164a169252aac8182b55
DaveFont19 <davidfontes20@gmail.com>
2023-10-18 14:30:56 -0600

    Nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import Form from "./components/formulariopost"
+
+import AppRouter from './AppRouter';
 
 export default function App() {
   return (
     <>
-      <Form/>
+    <AppRouter/>
     </>
   )
 }
---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import logofunval from "../assets/logofunval.png";
+import { Link } from "react-router-dom";
+
+import Avatar from "@mui/material/Avatar";
+import Stack from "@mui/material/Stack";
+import Badge from "@mui/material/Badge";
+import MailIcon from "@mui/icons-material/Mail";
+
+function Dashboard() {
+  return (
+    <nav className="w-screen flex justify-between items-center mt-4">
+      <div className="flex items-center">
+        <Link to="/">
+          <img src={logofunval} className="h-[40px]"></img>
+        </Link>
+        <div className="ml-20 gap-5 flex">
+          <Link to="/" className="font-bold">
+            Pagina Principal
+          </Link>
+          <Link to="/area-personal">Área Personal</Link>
+          <Link to="/posts">Posts</Link>
+        </div>
+      </div>
+      <div>
+        <Stack spacing={4} direction="row" sx={{ color: "action.active" }} className="flex items-center mr-12">
+          <Badge color="secondary" badgeContent={0} showZero>
+            <MailIcon />
+          </Badge>
+          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        </Stack>
+      </div>
+    </nav>
+  );
+}
+
+export default Dashboard;
